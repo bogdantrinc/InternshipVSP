d3e9c51dc8149c30a7746cbdb0f220b701c63619
Bogdan <bogdan.trinc@student.upt.ro>
2022-09-18 16:46:15 +0300

    ...

--- Project_1/snippets/views.py
+++ Project_1/snippets/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
